0a383c198b6176592f35c9075506fdacce2a5a56
3went2 <webb.png@gmail.com>
2017-10-10 18:02:55 +0800

    fix x-var bug

--- src/utils/convertXKey.js
+++ src/utils/convertXKey.js
@@ -1,6 +1,6 @@
 
 import { forEach } from 'lodash';
-import { NAME, PARAM_VAR, MODEL, OPERATOR, COERCION } from '../constants';
+import { NAME, MODEL, OPERATOR, COERCION } from '../constants';
 
 export function convertXKey(key) {
 	switch (key) {
@@ -8,8 +8,6 @@ export function convertXKey(key) {
 			return NAME;
 		case 'model':
 			return MODEL;
-		case 'params':
-			return PARAM_VAR;
 		case 'operator':
 			return OPERATOR;
 		case 'coercion':
