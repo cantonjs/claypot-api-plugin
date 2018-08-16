50e301284cd13a51da7819e07843654a7947073d
3went2 <webb.png@gmail.com>
2018-08-16 19:54:59 +0800

    upgrade eslint

--- src/middlewares/access.js
+++ src/middlewares/access.js
@@ -1,30 +1,24 @@
-
-// import getParamValue from '../utils/getParamValue';
 import uniqLocations from '../utils/uniqLocations';
 import { find } from 'lodash';
 
 export default function accessMiddleware(securities) {
-	// if (securities.length) {
-	// 	securities = uniqLocations(securities).map((security) => {
-	// 		paramToIgnores.push(security);
-	// 		return security;
-	// 	});
-	// }
-
 	return async (ctx, next) => {
 		if (securities.length) {
+			const verifies = uniqLocations(securities)
+				.map((security) => {
+					const param = find(
+						ctx.clay.__params,
+						({ spec }) =>
+							spec.in === security.in && spec.name === security.name,
+					);
 
-			const verifies = uniqLocations(securities).map((security) => {
-				const param = find(ctx.clay.__params, ({ spec }) =>
-					spec.in === security.in && spec.name === security.name
-				);
+					// delete secerity param
+					delete ctx.clay.__params[param.name];
 
-				// delete secerity param
-				delete ctx.clay.__params[param.name];
-
-				const token = param.value;
-				return token && ctx.clay.verify(token);
-			}).filter(Boolean);
+					const token = param.value;
+					return token && ctx.clay.verify(token);
+				})
+				.filter(Boolean);
 
 			const decodes = await Promise.all(verifies);
 
