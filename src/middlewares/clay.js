aa21504ef950e895c3bde6b5c45b93cc94942eae
3went2 <webb.png@gmail.com>
2018-08-20 14:57:06 +0800

    fix ctx.redirect() bug

--- src/middlewares/clay.js
+++ src/middlewares/clay.js
@@ -1,4 +1,3 @@
-
 export default function clayMiddleware() {
 	return async (ctx, next) => {
 		const clay = {
--- src/middlewares/operation.js
+++ src/middlewares/operation.js
@@ -1,11 +1,9 @@
-
 import logger from '../utils/logger';
 
 export default function operationMiddleware(modelName, operator) {
 	return async (ctx) => {
-
 		logger.trace(
-			`handling "${ctx.originalUrl}" by model: ${modelName}.${operator}`
+			`handling "${ctx.originalUrl}" by model: ${modelName}.${operator}`,
 		);
 
 		const model = ctx.clay.models[modelName];
@@ -20,9 +18,7 @@ export default function operationMiddleware(modelName, operator) {
 			return ctx.throw(500);
 		}
 
-		ctx.body = await model[operator](
-			ctx.clay.params,
-			ctx,
-		);
+		const body = await model[operator](ctx.clay.params, ctx);
+		if (body) ctx.body = body;
 	};
 }
