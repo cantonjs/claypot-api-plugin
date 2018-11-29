eb44b8174d4bb3488eeb05e890703d3bd057dd81
Webb Peng <webb.png@gmail.com>
2018-11-29 22:35:06 +0800

    remove error.stack in logs

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -9,12 +9,14 @@ export default function error() {
 		}
 		catch (err) {
 			Object.assign(err, { url: originalUrl, method });
+			const { stack } = err;
+			Reflect.deleteProperty(err, 'stack');
 			logger.error(err);
 			const { code = 1, message, status = 400 } = err;
 			ctx.status = status;
 			const body = { code, message };
 			if (err.reason) body.reason = err.reason;
-			if (isDev) body.stack = err.stack;
+			if (isDev) body.stack = stack;
 			ctx.body = body;
 		}
 	};
